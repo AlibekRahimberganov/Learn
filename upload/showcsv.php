be8fe812c0a1e69ffa8e1ef93a90c10a0375bf6c
AlibekRahimberganov <136824626+AlibekRahimberganov@users.noreply.github.com>
2025-08-07 11:15:37 +0500

    errors fixed showcsv.php

--- upload/showcsv.php
+++ upload/showcsv.php
@@ -1,46 +1,7 @@
-<?php 
-    include 'upload.php';
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    if(isset($_FILES['data']))
-    {
-        if($_FILES['data']['type'] == 'text/csv')
-        {
-            $handle = fopen($_FILES['data']['tmp_name'] ,'r');
-            if($handle != false)
-            {
-                echo "<table>";
-                echo "<tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>E-mail</th>
-                    <th>Phone Number</th>
-                    </tr>";
-
-                while(($info = fgetcsv($handle, 0, ",")) !== false)
-                {
-                    echo "<tr>";
-                    foreach($info as $text)
-                    {
-                        if($text != NULL)
-                        echo "<td>" . htmlspecialchars($text) . "</td>";
-                    }
-                    echo "</tr>";
-                }
-                echo "</table>";
-                fclose($handle);
-            }
-        }
-        else
-        {
-            print("File is not a CSV!");
-        }
-    }
-?>
-
 <html>
-<body>
-<style>
+<head>
+    <title> Show CSV </title>
+    <style>
     table, tr, th, td{
         border: 1px solid black;
         border-collapse: collapse;
@@ -56,8 +17,45 @@
         width: auto;
     }
     body{
-        background-color: lightblue;
+        background-color: lightgray;
     }
 </style>
+</head>
+<body>
+<?php 
+    if(isset($_FILES['data']))
+    {
+        if($_FILES['data']['type'] == 'text/csv')
+        {
+            $handle = fopen($_FILES['data']['tmp_name'] ,'r');
+            if($handle != false)
+            {
+                echo "<table>";
+                echo "<tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Phone Number</th>
+                    </tr>";
+
+                while(($info = fgetcsv($handle, 1000, ",")) !== false)
+                {
+                    echo "<tr>";
+                    foreach($info as $text)
+                    {
+                        if($text != NULL)
+                        echo "<td>" . htmlspecialchars($text) . "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+        }
+        else
+        {
+            print("File is not a CSV!");
+        }
+    }
+?>
 </body>
 </html>
