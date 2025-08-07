6ff1dcf4f958b335686dce18b3cf1945f71a17bc
AlibekRahimberganov <136824626+AlibekRahimberganov@users.noreply.github.com>
2025-08-07 11:25:56 +0500

    syntax has been corrected pt2 upload.php

--- upload/upload.php
+++ upload/upload.php
@@ -1,18 +1,19 @@
-<html>
-<head lang="eng-us">
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <title> Upload CSV </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8"/>    
     <style>
-    body{
-        background-color: lightblue;
-    }
-</style>
+        body{
+            background-color: lightblue;
+        }
+    </style>
 </head>
 <body>
     <form method="POST" enctype="multipart/form-data">
         <label> Choose the file
-            <input type="file" name="data">
+            <input type="file" name="data"/>
         </label>
         <button type="submit"> Upload </button>
     </form>    
